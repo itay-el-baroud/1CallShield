7827e294f1ff98af6c5ba5e30159d95b0dd3ddc2
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-05 18:00:33 +0300

    Add color definitions for light and dark themes

---
+++ app/src/main/java/com/callshield/ui/theme/Color.kt
@@ -0,0 +1,34 @@
+package com.callshield.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val LightBackground = Color(0xFFF5F5F5)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightOnSecondary = Color(0xFF332D41)
+val LightOnTertiary = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF1C1B1F)
+val LightOnSurface = Color(0xFF1C1B1F)
+
+// Dark Theme
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnPrimary = Color(0xFF381E72)
+val DarkOnSecondary = Color(0xFFCCC2DC)
+val DarkOnTertiary = Color(0xFF492532)
+val DarkOnBackground = Color(0xFFE6E1E5)
+val DarkOnSurface = Color(0xFFE6E1E5)
+
+// Custom Colors
+val BlockRed = Color(0xFFE53935)
+val BlockRedLight = Color(0xFFEF5350)
+val SuccessGreen = Color(0xFF43A047)
+val WarningOrange = Color(0xFFFB8C00)
+val InfoBlue = Color(0xFF1E88E5)
